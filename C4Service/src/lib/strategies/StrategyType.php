a00b0ae33f1e1ec59c13f6d8354aab11ca5d7b71
nicholemaldonado4 <43501948+nicholemaldonado4@users.noreply.github.com>
2021-12-30 00:50:34 -0700

    Update StrategyType.php

--- C4Service/src/lib/strategies/StrategyType.php
+++ C4Service/src/lib/strategies/StrategyType.php
@@ -2,7 +2,6 @@
 // Nichole Maldonado
 // Lab 1 - StrategyType
 // September 15, 2020
-// Dr. Cheon, CS3360
 // Contains a constant array that maps the strategy name with its corresponding
 // class name.
 
